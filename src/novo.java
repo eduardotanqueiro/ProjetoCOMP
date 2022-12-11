9230e46da2bf1d4977cde52b3ea5c241a4dcfc23
RicardoSilv28 <ricardosilv528@gmail.com>
2022-12-11 20:51:58 +0000

    Final, get_reallit change

--- src/novo.java
+++ src/novo.java
@@ -7,11 +7,14 @@
     //     if(c(5)){}
     // }
     public static double a() {
-        if(!c(5)){}
+
+        double a;
+        a = 1231231231231232132123218312893712893798127381273891273891273891279813123213123.2;
+        return a;
     }
-    public static int c(int h) {
-        return;
-    }
+    // public static int c(int h) {
+    //     return;
+    // }
 
 //     public static void a() {
 //         boolean b;
